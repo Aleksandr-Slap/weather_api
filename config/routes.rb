b15a4ea76372520b775b5d7a6db3365de5488f50
KALi <aleksandr-slap@rambler.ru>
2023-03-03 16:06:35 +0300

    corrected routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   namespace 'weather' do
-    namespace 'historical' do
-      get '/', to: 'weather#historical'
-      get '/max', to: 'weather#temp_max'
-    end
-    get '/current', to: 'weather#current'
-  end    
+    get 'current', to: 'weather#current'
+    get 'historical', to: 'weather#historical'
+    get 'historical/max', to: 'weather#temp_max'
+  end
 end
