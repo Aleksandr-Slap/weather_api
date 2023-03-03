eaf1c4ec0d03f60f2754ded554f500a3da8742e6
KALi <aleksandr-slap@rambler.ru>
2023-03-03 14:58:51 +0300

    an additional controller removed, the variables named correctly

--- app/controllers/weather/historical/weather_controller.rb
+++ app/controllers/weather/historical/weather_controller.rb
@@ -8,21 +8,21 @@ module Weather
       HISTORY_WEATHER_URL = "http://dataservice.accuweather.com/currentconditions/v1/295212/historical/24?#{ENV.fetch('API_KEY_WEATHER')}".freeze
 
 
-      # def current
-      #   response = HTTParty.get(CURRENT_WEATHER_URL)
-      #   body = JSON.parse(response.body)
-      #   value = body[0]['Temperature']['Metric']['Value']  #Экшн перенесён в controller/weather/weather_controller
+      def current
+        response = HTTParty.get(CURRENT_WEATHER_URL)
+        body = JSON.parse(response.body)
+        current_temp = body[0]['Temperature']['Metric']['Value']  #Экшн перенесён в controller/weather/weather_controller
 
-      #   render json: value
-      # end
+        render json: current_temp
+      end
 
       def historical
         response = HTTParty.get(HISTORY_WEATHER_URL)
         body = JSON.parse(response.body)
-        historical_values = {}
-        body.each { |e| historical_values[e['LocalObservationDateTime'].to_time] =e['Temperature']['Metric']['Value'] }
+        hourly_temp = {}
+        body.each { |e| hourly_temp[e['LocalObservationDateTime'].to_time] =e['Temperature']['Metric']['Value'] }
         
-        render json: historical_values
+        render json: hourly_temp
       end
 
       def temp_max
