8a65c38aa3d010cca528c7a959e9c9719e5992d4
KALi <aleksandr-slap@rambler.ru>
2023-03-03 21:44:48 +0300

    robocop fixes

--- app/controllers/api/v1/weather_controller.rb
+++ app/controllers/api/v1/weather_controller.rb
@@ -1,17 +1,17 @@
+# frozen_string_literal: true
+
 module Api
   module V1
     class WeatherController < ApplicationController
-
       include HTTParty
 
       CURRENT_WEATHER_URL = "http://dataservice.accuweather.com/currentconditions/v1/295212?#{ENV.fetch('API_KEY_WEATHER')}".freeze
       HISTORY_WEATHER_URL = "http://dataservice.accuweather.com/currentconditions/v1/295212/historical/24?#{ENV.fetch('API_KEY_WEATHER')}".freeze
 
-
       def current
         response = HTTParty.get(CURRENT_WEATHER_URL)
         respone_body = JSON.parse(response.body)
-        current_temp = respone_body[0]['Temperature']['Metric']['Value']  #Экшн перенесён в controller/weather/weather_controller
+        current_temp = respone_body[0]['Temperature']['Metric']['Value']  # Экшн перенесён в controller/weather/weather_controller
 
         render json: current_temp
       end
@@ -20,14 +20,16 @@ module Api
         response = HTTParty.get(HISTORY_WEATHER_URL)
         respone_body = JSON.parse(response.body)
         hourly_temp = {}
-        respone_body.each { |_1| hourly_temp[_1['LocalObservationDateTime'].to_time] =_1['Temperature']['Metric']['Value'] }
-        
+        respone_body.each do |_1|
+          hourly_temp[_1['LocalObservationDateTime'].to_time] = _1['Temperature']['Metric']['Value']
+        end
+
         render json: hourly_temp
       end
 
       def temp_max
-        render json: "#{ENV}".class
-      end  
+        render json: ENV.to_s.class
+      end
     end
-  end  
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
       get 'weather/current', to: 'weather#current'
       get 'weather/historical', to: 'weather#historical'
       get 'weather/historical/max', to: 'weather#temp_max'
-    end  
+    end
   end
 end
